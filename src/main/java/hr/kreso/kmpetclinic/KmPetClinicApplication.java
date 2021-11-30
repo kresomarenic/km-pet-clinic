5b456fa8787b5d5c4c8329978ca3e3e518b6433a
kmarenic <kresimir.marenic@biss.hr>
2021-11-30 14:22:32 +0100

    Initial project add

---
+++ src/main/java/hr/kreso/kmpetclinic/KmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package hr.kreso.kmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmPetClinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KmPetClinicApplication.class, args);
+  }
+
+}
---
+++ src/test/java/hr/kreso/kmpetclinic/KmPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kreso.kmpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmPetClinicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
